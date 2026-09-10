b38e0bb69ddf00141f2bb15edd84b8f3c4dbe8d8
bigboardcc <bigboardcc@yahoo.com>
2026-09-10 13:59:56 -0400

    update

--- main.js
+++ main.js
@@ -6,23 +6,25 @@ async function playerLinks(){
         const row = e.target.closest('.player-row');
         if (!row) return;
         e.preventDefault();
-        const player = row.dataset.player;
-        const url = `/players/${player.replace(/ /g,'-').replace(/'/g,'').toLowerCase()}.html`;
-        const response = await fetch(url);
-        if (!response.ok) {
-            throw new Error(`HTTP ${response.status}`);
-        }
-        const html = await response.text();
         // Create widget
         const widget = document.createElement('div');
         widget.classList.add('player-widget-container');
         widget.innerHTML = `
             <div class="player-widget-box">
                 <div class="player-widget-close">X</div>
-                <div class="player-widget-content">${html}</div>
+                <div class="player-widget-content" id="widgetContent">
+                    <div class="loading-small"><img src="img/loading.gif"></div>
+                </div>
             </div>
             `;
         document.body.appendChild(widget);
+        // Fetch player data
+        const player = row.dataset.player;
+        const url = `/players/${player.replace(/ /g,'-').replace(/'/g,'').toLowerCase()}.html`;
+        const response = await fetch(url);
+        if (!response.ok) throw new Error(`HTTP ${response.status}`);
+        const html = await response.text();
+        document.querySelector('#widgetContent').innerHTML = html;
         // Close widget
         document.querySelector('.player-widget-container').addEventListener('click', async(e) =>{
             const target = e.target;
